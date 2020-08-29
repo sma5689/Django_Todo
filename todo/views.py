511effde8c544615bdc6439626bdca7ac485a4f9
sma5689 <sma5689@yandex.ru>
2020-08-30 01:36:39 +0300

    Initial commit

---
+++ todo/views.py
@@ -0,0 +1,43 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import TodoForm
+from .models import Todo
+
+#CRUD - Create, Retrieve, Update, Delete, List
+
+def todo_list(request):
+	todos = Todo.objects.all()
+	context = {
+		"todo_list": todos
+	}
+	return render(request, "todo/todo_list.html", context)
+
+def todo_detail(request, id):
+	todo = Todo.objects.get(id=id)
+	context = {
+		"todo": todo
+	}
+	return render(request, "todo/todo_detail.html", context)
+
+def todo_create(request):
+	form = TodoForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		return redirect('/')
+	#	name = form.cleaned_data['name']
+	#	due_date = form.cleaned_data['due_date']
+	#	print(name, due_date)
+		
+	#	new_todo = Todo.objects.create(name=name, due_date=due_date)
+
+	context = {"form": form}
+	return render(request, "todo/todo_create.html", context)
+
+def todo_update(request, id):
+	todo = Todo.objects.get(id=id)
+	form = TodoForm(request.POST or None, instance=todo)
+	if form.is_valid():
+		form.save()
+		return redirect('/')
+	context = {"form": form}
+	return render(request, "todo/todo_update.html", context)
