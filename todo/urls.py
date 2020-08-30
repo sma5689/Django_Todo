68863c1546cda3c020151c75473d06f2b8d3dcff
sma5689 <sma5689@yandex.ru>
2020-08-31 02:23:40 +0300

    Added bootstrap styling

--- todo/urls.py
+++ todo/urls.py
@@ -5,6 +5,7 @@ from .views import (
 	todo_detail,
 	todo_create,
 	todo_update,
+	todo_delete,
 )
 
 app_name = 'todos'
@@ -14,5 +15,5 @@ urlpatterns = [
 	path('create/', todo_create),
 	path('<id>/', todo_detail),
 	path('<id>/update/', todo_update),
-
+	path('<id>/delete/', todo_delete),
 ]
--- todo/views.py
+++ todo/views.py
@@ -41,3 +41,8 @@ def todo_update(request, id):
 		return redirect('/')
 	context = {"form": form}
 	return render(request, "todo/todo_update.html", context)
+
+def todo_delete(request, id):
+	todo = Todo.objects.get(id=id)
+	todo.delete()
+	return redirect('/')
